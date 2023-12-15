832b665868d24b10ba737606632e89d83be6f5fb
Phinyo <644230059@webmail.npru.ac.th>
2023-12-15 14:21:24 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.header("Phinyo Sabai")
+
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('ายภิญโญ สบาย')
+st.subheader('สาขาเทคโนโลยีสารสนเทศ')
+st.markdown("----")
